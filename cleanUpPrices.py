15592be9f0ad6e141cc84cacecbe7aa9ae6babb5
Chandler Ferry <chandlerferry@gmail.com>
2024-03-27 15:20:00 -0700

    feat: python cleanUpPrices

---
+++ cleanUpPrices.py
@@ -0,0 +1,39 @@
+import json
+from pathlib import Path
+
+# Union Weight as fallback
+DEFAULT_WEIGHT = 4987
+PRICE_FLOOR = 6
+
+cardData = './data/cards.json'
+cardPriceOverrides = './data/overrides.json'
+
+with open(cardData, 'r') as file:
+    allCards = json.load(file)
+with open(cardPriceOverrides, 'r') as file:
+    cardPriceOverrides = json.load(file)
+
+clean_card_data = []
+
+for card in allCards:
+    override = next((o for o in cardPriceOverrides if o['cardName'] == card['name']), None)
+
+    if card.get('reward') == 'Disabled':
+        continue
+
+    if override:
+        card['price'] = override['cardValue']
+    elif card['price'] < PRICE_FLOOR:
+        card['price'] = 0
+
+    if 'drop' not in card:
+        continue
+    elif 'max_level' not in card['drop']:
+        card['drop']['max_level'] = 100
+
+    card.setdefault('weight', DEFAULT_WEIGHT)
+
+    clean_card_data.append(card)
+
+with open("prices.json", "w") as file:
+    json.dump(clean_card_data, file)
